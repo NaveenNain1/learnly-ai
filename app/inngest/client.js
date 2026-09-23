f9752658c05c37d4d049f041cef9040a26c13365
NaveenNain1 <nainnaveen24@gmail.com>
2026-09-23 12:10:00 +0530

    feat(ai): integrate Google Gemini 2.5 Flash for course outline generation

---
+++ app/inngest/client.js
@@ -0,0 +1,8 @@
+import { Inngest } from "inngest";
+ 
+
+export const inngest = new Inngest({
+ id: "ai-lms",
+  eventKey:
+    "Jb1K9KZCNwaCm1SPvkPv3cvBvfydoML-qIMQ5rT40sWcvcxokGafYZslg8rZW7Nl1JCzeqVe7YzeNEfSK4bdvg",
+});
